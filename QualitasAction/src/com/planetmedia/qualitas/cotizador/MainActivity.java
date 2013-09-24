002ba41159174fbd70a96176b00faae0c508aa42
alanCedeno <alandan15@hotmail.com>
2013-09-24 13:18:34 -0500

    Qualitas con Action bar

---
+++ QualitasAction/src/com/planetmedia/qualitas/cotizador/MainActivity.java
@@ -0,0 +1,33 @@
+package com.planetmedia.qualitas.cotizador;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+	        case R.id.menu:
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+		}
+	}
+}
